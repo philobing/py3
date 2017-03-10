42d83d3f40f54f7dfde996a919d3b9e4735d5614
philobing <philobing@163.com>
2017-03-10 12:21:32 +0800

    三级菜单的循环控制

--- xiti/three.menu.py
+++ xiti/three.menu.py
@@ -29,7 +29,24 @@ data = {
 while True:
     for i in data:
         print(i)
-    choice = input("选择进入>>:")
+    choice = input("选择进入1>>:")
     if choice in data:
         while True:
-            print("hello")
+            for i2 in data[choice]:
+                print("\t", i2)
+            choice2 = input("选择进入>>2:")
+            if choice2 in data[choice]:
+                while True:
+                    for i3 in data[choice][choice2]:
+                        print("\t\t", i3)
+                    choice3 = input("选择进入>>3:")
+                    if choice3 in data[choice][choice2]:
+                        for i4 in data[choice][choice2][choice3]:
+                            print("\t\t", i4)
+                        choice4 = input("最后一层,按b返回>>:")
+                        if choice4 == "b":
+                            pass
+                    if choice3 == "b":
+                        break
+            if choice2 == "b":
+                break
